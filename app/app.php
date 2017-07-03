9124c71391a67fee5b9ccf68055c641003f23091
jacob ruleaux and Steve Spitz <steve@>
2017-07-03 11:49:23 -0700

    Add everything, non functional

--- app/app.php
+++ app/app.php
@@ -4,9 +4,15 @@
     require_once __DIR__."/../src/Src.php";
 
     session_start();
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    if(empty($_SESSION['ping_pong'])) {
+    $_SESSION['ping_pong'] = array();
+}
 
     $app = new Silex\Application();
-
+$app['debug'] = true;
     $app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"
     ));
 
@@ -14,8 +20,17 @@
         return $app['twig']->render('form.html.twig');
     });
 
+    $app->post("/output", function() use ($app) {
+        $pingpossum = new PingPonginator($_POST['pong']);
+        $pingpossum->makePongHappen();
+        $pingpossum->save();
+        // var_dump($pingpossum);
+        return $app['twig']->render('ping.html.twig', array("pings" => $pingpossum));
+    });
+
     $app->get("/output", function() use ($app) {
-        return $app['twig']->render('ping.html.twig');
+        // var_dump($pingpossum);
+        return $app['twig']->render('ping.html.twig', array("pings" => PingPonginator::getAll()));
     });
 
 return $app;
--- src/Src.php
+++ src/Src.php
@@ -2,18 +2,42 @@
 
 class PingPonginator
 {
-    function makePongHappen($input_ping)
+    public $input;
+    public $output;
+
+    function __construct($input)
+    {
+        $this->input = $input;
+        $this->output;
+    }
+
+    function makePongHappen()
     {
         $output_pong = array();
-        for ($x = $input_ping; $x > 0; --$x) {
-            array_push($output_pong, $x);
-        }
-        foreach($output_pong as $pong) {
-            if ($pong % 3 == 0) {
-                $pong = "Ping";
+        for ($x = 1; $x <= $this->input; ++$x) {
+            if ($x % 15 == 0 && $x > 15) {
+                array_push($output_pong, "Ping-Pong");
+            } elseif ($x % 5 == 0) {
+                array_push($output_pong, "Pong");
+            } elseif ($x % 3 == 0) {
+                array_push($output_pong, "Ping");
+            } else {
+                array_push($output_pong, $x);
             }
         }
-        return $output_pong;
+         $this->output = $output_pong;
+    }
+    function printOutput()
+    {
+        return $this->output;
+    }
+    function save()
+    {
+        array_push($_SESSION['ping_pong'], $this);
+    }
+    static function getAll()
+    {
+        return $_SESSION['ping_pong'];
     }
 }
 
